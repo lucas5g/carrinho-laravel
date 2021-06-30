125846e7076eb86c050923eeb4dac1fd1939a4e4
Lucas de Sousa Assunçao <lucasdesousa19@hotmail.com>
2021-06-30 10:13:07 -0300

    first commit

---
+++ app/Http/Controllers/Core/StoreController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Core;
+
+use App\Http\Controllers\Controller;
+use App\Models\Core\Store;
+use Illuminate\Http\Request;
+
+class StoreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $stores = Store::get();
+        return $stores;
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        return Store::create($data);
+
+        // return $store;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_06_30_120854_create_stores_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_fantasya', 100);
+            $table->string('cnpj', 25)->nullable();
+            $table->string('street', 100)->nullable();
+            $table->string('number', 10)->nullable();
+            $table->string('complement', 30)->nullable();
+            $table->string('city', 20)->nullable();
+            $table->string('state', 10)->nullable();
+            $table->string('cellphone', 10)->nullable();
+            $table->string('site', 10)->nullable();
+            $table->string('email', 10)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
